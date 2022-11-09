912bc35d6abeddcff64db7e8776019ee67b30c17
auvm <angelurielvelascom@gmail.com>
2022-11-08 20:19:02 -0600

    first example using production and development mode

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// importamos funcion sum 
+import sum from './utils/functions.js'
+
+
+// primer ejemplo
+const hello = "hello";
+console.log(hello);
+
+
+// segundo ejemplo
+console.log(sum(2,2));
+
